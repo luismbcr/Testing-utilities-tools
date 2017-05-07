0a6b1648ac6d736343bd604e1e75263a3d379add
Luis Montero <luismb@luismb-thi-m.local>
2017-05-07 16:14:32 -0600

    Compare module functionallity- Close #4

--- app/modules/compare.js
+++ app/modules/compare.js
@@ -1,5 +1,19 @@
+import 'colors';
+import * as jsDiff from 'diff';
+
 module.exports = {
-    example: ()=>{
-        return 'example';
+    textDiff: (a,b)=>{
+        let diff = jsDiff.diffWords(a, b);
+        console.log('--- Content Comparison Result ---'.bgBlue.white);
+        console.log('   Additions'.green + ' Common'.white + ' Deletions'.red);
+        console.log('---------------------------------'.bgBlue.white+'\n');
+        diff.forEach(function(part){
+            // green for additions, red for deletions
+            // white for common parts
+            let color = part.added ? 'green' :
+                part.removed ? 'red' : 'white';
+            process.stderr.write(part.value[color]);
+        });
+        process.stderr.write('\n');
     }
 }
